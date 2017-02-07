1dd8ce9186077ea1d6f62888352b1062e9654a7c
Talbot <talbotlawrence75@gmail.com>
2017-02-07 12:03:15 -0600

    got the initial setup

---
+++ javascripts/sandwichmaker.js
@@ -0,0 +1,11 @@
+"use strict";
+
+var SandwichMaker = (function() {
+  var totalPrice = 0;
+
+  return {
+    addTopping: function(toppingPrice) {
+      totalPrice += toppingPrice;
+    }
+  };
+})();
